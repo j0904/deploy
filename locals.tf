30d69a011b8abd83d653a716b6299eb7220471f6
cui <bigtangle@bigt.ai>
2025-05-26 19:40:25 +0200

    path

--- locals.tf
+++
@@ -1,9 +0,0 @@
-# Dictionary Locals
-locals {
-  compute_flexible_shapes = [
-"VM.Standard.E2.1.Micro"
-   
-  ]
-  is_flexible_shape = contains(local.compute_flexible_shapes, var.Shape)
-  default_availability_domain = lookup(data.oci_identity_availability_domains.ADs.availability_domains[0], "name", "")
-}
