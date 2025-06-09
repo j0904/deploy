3102bfd32e722771615393088022eac4ca3a565c
cui <bigtangle@bigt.ai>
2025-06-09 17:22:55 +0200

    split bluesky service

--- oraclevm/vm1/locals.tf
+++ oraclevm/vm1/locals.tf
@@ -1,9 +1,4 @@
 # Dictionary Locals
-locals {
-  compute_flexible_shapes = [
-"VM.Standard.E2.1.Micro"
-   
-  ]
-  is_flexible_shape = contains(local.compute_flexible_shapes, var.Shape)
+locals { 
   default_availability_domain = lookup(data.oci_identity_availability_domains.ADs.availability_domains[0], "name", "")
 }
